edd655d7e976e035fa5b321c1c680cf75e378dfd
SuperSpaceZova <ravalico.giovanni@gmail.com>
2017-11-28 20:54:31 +0100

    details styled

--- client/src/article-details/article-details.js
+++ client/src/article-details/article-details.js
@@ -6,23 +6,14 @@ import { connect } from 'react-redux';
 
 // UTILS
 import moment from 'moment';
+import './article-detail.css';
 
 // MATERIAL-UI
-import { withStyles } from 'material-ui/styles';
-import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import Grid from 'material-ui/Grid';
-import Divider from 'material-ui/Divider';
-import Paper from 'material-ui/Paper';
 
-const styles = theme => ( {
-    card: {
-        // maxWidth: 345
-    },
-    media: {
-    }
-} );
+
 
 class ArticleDetails extends Component {
 
@@ -40,29 +31,32 @@ class ArticleDetails extends Component {
             urlToImage,
             url
         } = this.props.article;
+
         return (
             <div>
-                <Paper>
-                    <Grid container direction='column' justify='flex-start' alignItems='stretch' spacing={0}>
+                <Grid container direction='column' justify='flex-start' alignItems='stretch' spacing={0}>
 
-                        <Typography type='headline' component='h1'>
-                            {title}
-                        </Typography>
-                        <Divider />
+                    {/* HEADER */}
+                    <header className='header'>
+                        <h1 className='title'>{title}</h1>
+                        <hr className='divider'/>
+                        <h2 className='author'>by {author}</h2>
+                        <h3 className='publishedAt'>{moment(publishedAt).format('ddd, MMM Do')}</h3>
+                    </header>
 
-                        <Typography type='subheading' component='h2'>
-                            {author}
-                        </Typography>
+                    {/* IMAGE */}
+                    <div className='img-wrapper'>
+                        <img className='img' src={urlToImage} alt={title} />
+                    </div>
 
-                        <Typography type='subheading' component='h3'>
-                            {publishedAt}
-                        </Typography>
 
-                        <img src={urlToImage} alt={title} />
+                    {/* DESCRIPTION */}
+                    <div className='descr-wrapper'>
+                        <p className='descr'>{description}</p>
+                    </div>
 
-                        <Typography component='p'>
-                            {description}
-                        </Typography>
+                    {/* NAVIGATION */}
+                    <div className='action-wrapper'>
                         <Button
                             dense
                             color='accent'
@@ -74,10 +68,10 @@ class ArticleDetails extends Component {
                             color='primary'
                             href={url}
                             target='_blank'>
-                            Go to Article
+                            Read More @{source.name}
                         </Button>
-                    </Grid>
-                </Paper>
+                    </div>
+                </Grid>
             </div>
         );
     }
@@ -88,4 +82,4 @@ class ArticleDetails extends Component {
 const mapStateToProps = (state, ownProps) => ({
     article: state.news.items && state.news.items[ownProps.match.params.artId]
 });
-export default connect( mapStateToProps )( withStyles(styles)(ArticleDetails) );
+export default connect( mapStateToProps )(ArticleDetails);
