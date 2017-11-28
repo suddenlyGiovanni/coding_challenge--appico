ae904e5a61d1a4bc044ffc72cffac133ad3428a5
SuperSpaceZova <ravalico.giovanni@gmail.com>
2017-11-28 16:11:03 +0100

    stiles changes article component

--- client/src/article-details/article-details.js
+++ client/src/article-details/article-details.js
@@ -4,19 +4,23 @@ import {Link} from 'react-router-dom';
 // REDUX
 import { connect } from 'react-redux';
 
+// UTILS
+import moment from 'moment';
+
 // MATERIAL-UI
 import { withStyles } from 'material-ui/styles';
 import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
-
+import Grid from 'material-ui/Grid';
+import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper';
 
 const styles = theme => ( {
     card: {
-        maxWidth: 345
+        // maxWidth: 345
     },
     media: {
-        height: 200
     }
 } );
 
@@ -30,6 +34,7 @@ class ArticleDetails extends Component {
         const {
             title,
             author,
+            publishedAt,
             description,
             source,
             urlToImage,
@@ -37,30 +42,33 @@ class ArticleDetails extends Component {
         } = this.props.article;
         return (
             <div>
-                <Card className={classes.card}>
-                    <CardMedia
-                        className={classes.media}
-                        image={urlToImage}
-                        img='img'
-                    />
+                <Paper>
+                    <Grid container direction='column' justify='flex-start' alignItems='stretch' spacing={0}>
 
-                    <CardContent>
-                        <Typography type='headline' component='h2'>
+                        <Typography type='headline' component='h1'>
                             {title}
                         </Typography>
+                        <Divider />
+
+                        <Typography type='subheading' component='h2'>
+                            {author}
+                        </Typography>
+
+                        <Typography type='subheading' component='h3'>
+                            {publishedAt}
+                        </Typography>
+
+                        <img src={urlToImage} alt={title} />
+
                         <Typography component='p'>
                             {description}
                         </Typography>
-                    </CardContent>
-                    <CardActions>
                         <Button
                             dense
                             color='accent'
                             onClick={()=>this.props.history.go(-1)}>
                             Back
                         </Button>
-
-
                         <Button
                             dense
                             color='primary'
@@ -68,9 +76,8 @@ class ArticleDetails extends Component {
                             target='_blank'>
                             Go to Article
                         </Button>
-
-                    </CardActions>
-                </Card>
+                    </Grid>
+                </Paper>
             </div>
         );
     }
